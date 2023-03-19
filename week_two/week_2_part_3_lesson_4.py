878909a1aea4055ddb151e770c4a003d5e0512da
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-03-20 00:36:14 +0300

    Week one compleat. Week two: first and second part compleat.

---
+++ week_two/week_2_part_3_lesson_4.py
@@ -0,0 +1,33 @@
+import time
+import math
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+try:
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # формула для вычисления ответа капчи
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+    # Нажать на кнопку
+    browser.find_element(By.CSS_SELECTOR, '[type="submit"]').click()
+    # Принять confirm
+    alert = browser.switch_to.alert
+    alert.accept()
+    # На новой странице решить капчу для роботов, чтобы получить число с ответом
+    x = browser.find_element(By.ID, 'input_value').text
+    y = calc(x)
+    input_answer_element = browser.find_element(By.ID, 'answer')
+    input_answer_element.send_keys(y)
+    # Нажать на кнопку Submit
+    browser.find_element(By.CLASS_NAME, 'btn.btn-primary').click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
